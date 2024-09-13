8525f4bb33badea1dc244119405781f972b85773
Daniel A. Galindo Reyes <129880574+DanielGalindoDev@users.noreply.github.com>
2024-09-12 22:20:36 -0600

    Update test.c

--- test.c
+++ test.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lex.h"
 
-#define MAX_BUFFER 256  
+#define MAX_BUFFER 256
 
-int main(int argc,char* argv[])
-{
-    
+int main(int argc, char *argv[]) {
     unsigned char buffer[MAX_BUFFER];
-    FILE *pf;
-    pf = fopen(argv[1],"r");
+    FILE *pf = NULL;
 
-    if (pf == NULL)
-    {
-        puts("Error al abrir el archivo");
-        return 1;
+    if (argc > 1) {
+        pf = fopen(argv[1], "r");
+        if (pf == NULL) {
+            puts("Error opening the file, reading from console.");
+            pf = NULL;
+        }
+    } else {
+        puts("No file provided, reading from console.");
     }
-    
-    int tokenNum = 0; 
-    while (fgets(buffer,MAX_BUFFER,pf))
-    {
-        lexer l;
-        lexicalAnalyzer(buffer,&l);
-        if (l.token_count > 0) printf("\n\t----------------\n");
-        for (int i = 0; i < l.token_count; i++)
-        printTokenInfo(buffer,l.tokens[i]);
-        if (l.token_count > 0) printf("\t----------------\n");
-        tokenNum += l.token_count;
+
+    int tokenNum = 0;
+
+    if (pf) {
+        while (fgets(buffer, MAX_BUFFER, pf)) {
+            lexer l;
+            lexicalAnalyzer(buffer, &l);
+            if (l.token_count > 0) printf("\n\t----------------\n");
+            for (int i = 0; i < l.token_count; i++)
+                printTokenInfo(buffer, l.tokens[i]);
+            if (l.token_count > 0) printf("\t----------------\n");
+            tokenNum += l.token_count;
+        }
+        printf("\nToken Count: %d\n", tokenNum);
+        fclose(pf);
+    } else {
+        printf("Enter lines of text. To finish, enter a line with only the '?' character:\n");
+        while (fgets(buffer, MAX_BUFFER, stdin)) {
+            if (strchr(buffer, '?') != NULL && strlen(buffer) == 2) {
+                break;
+            }
+            lexer l;
+            lexicalAnalyzer(buffer, &l);
+            if (l.token_count > 0) printf("\n\t----------------\n");
+            for (int i = 0; i < l.token_count; i++)
+                printTokenInfo(buffer, l.tokens[i]);
+            if (l.token_count > 0) printf("\t----------------\n");
+            tokenNum += l.token_count;
+        }
+        printf("\nToken Count: %d\n", tokenNum);
     }
-    printf("\nToken Count: %d\n",tokenNum);
-    fclose(pf);
+
+    return 0;
 }
